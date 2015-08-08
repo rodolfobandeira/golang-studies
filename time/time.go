e77086c353edfee3c309690ad8d901c6b6061fc7
rodolfobandeira <contato@rodolfobandeira.com>
2015-08-08 13:24:13 -0400

    time

---
+++ time/time.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    fmt.Println("Bem-vindo ao playground!")
+
+    fmt.Println("A hora é", time.Now())
+}
+
+/*
+Bem-vindo ao playground!
+A hora é 2015-08-08 13:23:28.966998037 -0400 EDT
+*/
