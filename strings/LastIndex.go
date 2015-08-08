2e48ac76af04d74378f7debf7072ab88d70fbfcf
rodolfobandeira <contato@rodolfobandeira.com>
2015-08-08 13:18:53 -0400

    LastIndex

---
+++ strings/LastIndex.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    fmt.Println(strings.Index("go gopher", "go"))
+    fmt.Println(strings.LastIndex("go gopher", "go"))
+    fmt.Println(strings.LastIndex("go gopher", "rodent"))
+}
+
+// 0
+// 3
+// -1
+
