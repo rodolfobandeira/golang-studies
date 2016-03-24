375a387d219962e862da20c0c842d17362f6845e
rodolfobandeira <contato@rodolfobandeira.com>
2016-03-24 07:29:19 -0400

    examples tour - type conversions

--- tour/basic-types.go
+++ tour/basic-types.go
@@ -11,3 +11,14 @@ var (
     z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
+func main() {
+    const f = "%T(%v)\n"
+    fmt.Printf(f, ToBe, ToBe)
+    fmt.Printf(f, MaxInt, MaxInt)
+    fmt.Printf(f, z, z)
+}
+
+// bool(false)
+// uint64(18446744073709551615)
+// complex128((2+3i))
+
---
+++ tour/type-interface.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    v := 42.5 // change me!
+    // i := 42 // int
+    // f := 3.142 // float64
+    // g := 0.867 + 0.51 // complex128
+
+
+    fmt.Printf("v is of type %T\n", v)
+    // v is of type float64
+}
+
